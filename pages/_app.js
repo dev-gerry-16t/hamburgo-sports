a83502e0d9db73c6b071ab55f3d4a2853403747e
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2022-05-16 11:54:31 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+import "../styles/style.css";
+import "../styles/bootstrap.min.css";
+import "../styles/datepicker.css";
+import "../styles/fancybox.min.css";
+// import "../styles/font-awesome.min.css";
+import "../styles/mapboxstyle.css";
+import "../styles/swiper.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
